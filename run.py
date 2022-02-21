df24eb31c975e5b4d1f6d0ea27c1c67751f59e6b
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2022-02-21 12:48:10 +0300

    Fix run.py

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 import os
 
 with open("/model/run.sh", "w") as f:
-    videos = os.listdir("/dataset")
-    for video in videos:
-        f.write(f"mkdir /results/{video}\n")
-        f.write(f"python3 /model/main_test_vrt.py --task 001_VRT_videosr_bi_REDS_6frames --folder_lq /dataset/{video} --results /results/{video} --tile 6 128 128 --tile_overlap 2 20 20\n")
+    f.write(f"python3 /model/main_test_vrt.py --task 001_VRT_videosr_bi_REDS_6frames --folder_lq /dataset --results /results --tile 6 128 128 --tile_overlap 2 20 20\n")
 
 os.system("sh /model/run.sh")
