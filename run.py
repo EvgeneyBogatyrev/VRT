844dc5110129043ba612030ae33cfd149892402f
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2022-03-22 23:10:00 +0300

    Make VRT run with different options

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
 import os
+import json
+
+if os.path.isfile("/model/config.json"):
+    with open("/model/config.json", "r") as f:
+        data = json.load(f)
+    options = data['options']
+else:
+    options = "--tile 6 128 128 --tile_overlap 2 20 20"
 
 with open("/model/run.sh", "w") as f:
-    f.write(f"python3 /model/main_test_vrt.py --task 001_VRT_videosr_bi_REDS_6frames --folder_lq /dataset --results /results --tile 6 128 128 --tile_overlap 2 20 20\n")
+    f.write(f"python3 /model/main_test_vrt.py --task 001_VRT_videosr_bi_REDS_6frames --folder_lq /dataset --results /results {options}\n")
 
 os.system("sh /model/run.sh")
